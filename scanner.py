249e0c8d4699f1f8ea1e6fdf309798ba0affca3b
Akhil Manthina <akhilmanthina@gmail.com>
2023-09-21 20:31:09 -0700

    changed os command to subprocess

--- scanner.py
+++ scanner.py
@@ -1,14 +1,9 @@
-import os
+import subprocess
 
 def scanner(output_folder, input_file):
     print("Scanning pdf...")
     #print(os.getcwd())
-    command = f'./Audiveris/bin/Audiveris -batch -export -output {output_folder} {input_file}'
-    print(command)
-    os.system(command)
+    subprocess.run(['./Audiveris/bin/Audiveris', '-batch', '-export', '-output', output_folder, input_file])
     print("Done scanning")
 
 
-    
-if __name__ == '__main__':
-    scanner("./temp", "./carolflutecpy.pdf")
