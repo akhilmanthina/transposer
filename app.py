722ae1e12d3c21417de78ebfb1f6aba58567642d
Akhil Manthina <akhilmanthina@gmail.com>
2023-09-22 01:12:51 -0700

    cleaned up output pdf, removing temp files now in function

--- app.py
+++ app.py
@@ -36,6 +36,15 @@ def transpose_musicxml(file_path, input_instrument, output_instrument):
 
     return transposed_score
 
+def clear_temp(temp):
+    for filename in os.listdir(temp):
+        file_path = os.path.join(temp, filename)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+        except Exception as e:
+            print(f'Failed to delete {file_path}. Reason: {e}')
+
 
 if __name__ == '__main__':
     # Get the MusicXML file path and instrument names
@@ -43,6 +52,7 @@ if __name__ == '__main__':
     file_path = input("Enter the path of the pdf file: ").strip()
     input_instrument = input("Enter the input instrument name: ").strip()
     output_instrument = input("Enter the output instrument name: ").strip()
+
 
     scanner(temp, file_path)
     file_name = file_path.split("/")[-1].split(".")[0]
@@ -52,21 +62,20 @@ if __name__ == '__main__':
 
 
     # Transpose the MusicXML file
+    print("Transposing...")
     transposed_score = transpose_musicxml(mxl_file_path, input_instrument, output_instrument)
 
     # Save the transposed score to a new MusicXML file
     transposed_file_path = temp + file_name + "_transposed.mxl"
-    print(transposed_file_path)
+    #print(transposed_file_path)
+    print("Writing to mxl...")
     transposed_score.write('musicxml', transposed_file_path)
 
-    pdf = mxl_to_pdf(transposed_file_path)
+    print("Converting to pdf...")
+    mxl_to_pdf(transposed_file_path)
     
+    print("Cleaning up...")
+    #clear_temp(temp)
 
-    for filename in os.listdir(temp):
-        file_path = os.path.join(temp, filename)
-        try:
-            if os.path.isfile(file_path):
-                os.unlink(file_path)
-        except Exception as e:
-            print(f'Failed to delete {file_path}. Reason: {e}')
+    print("Done!")
 
--- generate_pdf.py
+++ generate_pdf.py
@@ -1,19 +1,17 @@
 import os
+import re
 import subprocess
+
 
 
 def mxl_to_pdf(mxl_file_path):
     mxl_to_ly_path = os.getcwd() + "/Lilypond/bin/musicxml2ly"
-
-
     ly_file = './temp/output.ly'
 
     subprocess.run([mxl_to_ly_path, "-o", ly_file, mxl_file_path])
     
-
+    modify_ly(ly_file)
     ly_to_pdf(ly_file)
-    os.remove(ly_file)
-
 
 
 def ly_to_pdf(ly_file):
@@ -23,4 +21,26 @@ def ly_to_pdf(ly_file):
     pdf_file = os.path.splitext(ly_file)[0] + '.pdf'
     
     
-    
+def modify_ly(ly_file):
+    with open(ly_file, 'r') as file:
+        content = file.read()
+
+    # Change the title
+    content = re.sub(r'(title\s*=\s*)"([^"]*)"', r'\1"Transposed Piece"', content)
+
+    # Remove the instrument naming
+    content = re.sub(
+        r'\\set Staff.instrumentName = "[^"]*"', 
+        '', 
+        content
+    )
+
+    content = re.sub(
+        r'\\set Staff.shortInstrumentName = "[^"]*"', 
+        '', 
+        content
+    )
+
+    # Write the modified content back to the file
+    with open(ly_file, 'w') as file:
+        file.write(content)
